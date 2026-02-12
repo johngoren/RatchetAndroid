16411c2e7cdf50387590b396ebd73502992c9718
consuelita <adelita@operatorfoundation.org>
2026-02-12 15:15:21 -0600

    Updated Dependencies

--- Ratchet/build.gradle.kts
+++ Ratchet/build.gradle.kts
@@ -36,8 +36,8 @@ android {
 
 dependencies {
     implementation("org.bouncycastle:bcprov-jdk18on:1.78")
-    implementation("com.github.OperatorFoundation:AESAndroid:v1.0.0")
-    implementation("com.github.OperatorFoundation:MADHAndroid:v1.0.4")
+    implementation("com.github.OperatorFoundation:AESAndroid:v1.0.1")
+    implementation("com.github.OperatorFoundation:MADHAndroid:v1.0.5")
     implementation("com.github.OperatorFoundation:Unishox2Android:v1.0.0")
 
     implementation(libs.androidx.core.ktx)
