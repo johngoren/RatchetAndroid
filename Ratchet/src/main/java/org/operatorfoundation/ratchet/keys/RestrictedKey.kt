5816ffa04215e4b10ca9e904d996e4adbf833457
John Gorenfeld <john@gorenfeld.net>
2026-09-21 14:38:24 +0100

    Restricted key class

--- Ratchet/src/main/java/org/operatorfoundation/ratchet/keys/RestrictedKey.kt
+++ Ratchet/src/main/java/org/operatorfoundation/ratchet/keys/RestrictedKey.kt
@@ -1,20 +1,47 @@
 package org.operatorfoundation.ratchet.keys
 
-import java.security.Security
 
-abstract class RestrictedKey(val bytes: ByteArray) {
+// TODO: Make bytes private and access only through use()
+// TODO: Make a copy of what is passed in.
+// TODO: The copyOf here returns a copy of a primitive, right? We can
+// assume that's the way Assured prefers copies to be consistently made
 
-    // TODO: Holds private value that can only be used once via "use"
-    // TODO: Zeroizes when closed.
+abstract class RestrictedKey(val bytes: ByteArray): AutoCloseable {
+
+    var isDestroyed: Boolean = false
+
+    fun use(block: (ByteArray)->Unit) {
+        check(!isDestroyed) { "Key has been destroyed "}
+        if (!isDestroyed) {
+            block(bytes)
+        }
+    }
+
+    fun copyBytes(): ByteArray {
+        check(!isDestroyed) { "Key has been destroyed "}
+        return bytes.copyOf()
+    }
+
+    override fun close() {
+        if (!isDestroyed) {
+            bytes.fill(0)
+            isDestroyed = true
+        }
+    }
+
+    override fun toString(): String {
+        return "Key redacted for security"
+    }
 
     override fun equals(other: Any?): Boolean
     {
-        throw SecurityException("No equality operations allowed")
+        throw SecurityException("Equality operations have been disabled for security")
     }
 
     override fun hashCode(): Int
     {
-        throw SecurityException("No hash codes allowed")
+        throw SecurityException("Hash codes have been disabled for security")
     }
+
 
 }
